1ce1da53729e0808cdffe01569886304c77c1436
白宇东 <summer88123@163.com>
2024-09-23 18:05:35 +0800

    临时适配鸿蒙

--- index.tsx
+++ index.tsx
@@ -1,8 +1,12 @@
-import {NativeModules} from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
 const {RNUdidGenerator} = NativeModules;
 
 function getUdid(parentDir: string) {
+    // fixme: 提供鸿蒙实现
+    if (Platform.OS === 'harmony') {
+        return Promise.resolve('123456');
+    }
     return RNUdidGenerator.getUdid(parentDir) as Promise<string>;
 }
 
